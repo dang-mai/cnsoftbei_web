3fc9ed04bf911d2c99c861f6b00fc5e09d08934b
True-False-debug <u202011967@hust.edu.cn>
2023-07-15 20:49:41 +0800

    3_模型推理.py

--- pages/3_模型推理.py
+++ pages/3_模型推理.py
@@ -41,4 +41,6 @@ def inference():
 st.button("开始推理", on_click=inference)
 process = st.empty()
 if st.session_state.predict is not None:
+    dataset_name_out = dataset_name + ".out"
     st.write(st.session_state.predict)
+    st.session_state.predict.to_csv(os.path.join(DATASET_DIR, dataset_name_out))
